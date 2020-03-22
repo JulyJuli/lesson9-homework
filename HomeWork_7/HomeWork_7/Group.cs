297c82c5ea1c6039c1c6f9000ab91ea189a42f96
Roman <roma.ne4aev2013@gmail.com>
2020-03-22 23:08:29 +0200

    HomeWork

---
+++ HomeWork_7/HomeWork_7/Group.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_7
+{
+    public class Group
+    {
+        public Group(string nameOfGroup,Student studentName,Teacher teacherName)
+        {
+            NameOfGroup = nameOfGroup;
+            Student StudentName = studentName;
+            Teacher TeacherName = teacherName;
+
+        }
+        public string NameOfGroup { get; set; }
+        public List<Student> Students { get; set; }
+        public List<Teacher> Teachers { get; set; }
+
+
+
+       
+    }
+
+
+}
